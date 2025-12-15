221c698d5364c59c5949a2f025d51cc7bd5d87c6
rajkori06 <raj060510@gmail.com>
2025-12-15 23:03:13 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,sum;
+
+    cout<<"enter the number";
+    cin>>a;
+
+    cout<<"enter the number";
+    cin>>b;
+
+    sum = a + b;
+     cout<<"the sum is "<<sum;
+
+     return 0;
+}
+
+
